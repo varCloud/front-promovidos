d7f316433eaafe0550925a72836a66473d691e8a
victor reyes <vreyes@grainchain.io>
2024-03-11 13:14:33 -0600

    se comenta el menu de la plantilla
    se comenta el menu de la plantilla

--- src/templates/layouts/Asides/DefaultAside.template.tsx
+++ src/templates/layouts/Asides/DefaultAside.template.tsx
@@ -19,21 +19,21 @@ import usersDb from '../../../mocks/db/users.db';
 const DefaultAsideTemplate = () => {
 	const navigate = useNavigate();
 
-	// return(
-	// 	<Aside>
-	// 		<AsideHead>
-	// 			<LogoAndAsideTogglePart />
-	// 		</AsideHead>
-	// 		<AsideBody>
-	// 			<Nav>
-	// 				<NavItem {...promovidosAllPages.dashboardPromodivosAppPages} />
-	// 				<NavItem {...promovidosAllPages.promotorAppPages} />
-	// 				<NavItem {...promovidosAllPages.promovidoAppPages} />
-	// 				<NavItem {...promovidosAllPages.enlaceAppPages} />
-	// 				</Nav>
-	// 		</AsideBody>
-	// 	</Aside>
-	// )
+	return(
+		<Aside>
+			<AsideHead>
+				<LogoAndAsideTogglePart />
+			</AsideHead>
+			<AsideBody>
+				<Nav>
+					<NavItem {...promovidosAllPages.dashboardPromodivosAppPages} />
+					<NavItem {...promovidosAllPages.promotorAppPages} />
+					<NavItem {...promovidosAllPages.promovidoAppPages} />
+					<NavItem {...promovidosAllPages.enlaceAppPages} />
+					</Nav>
+			</AsideBody>
+		</Aside>
+	)
 
 	return (
 		<Aside>
